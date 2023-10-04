01dd08ee4d9543636204c811eb5c2c3f507b5a30
Ben Coukos-Wiley <bjc8253@g.rit.edu>
2023-10-03 23:47:46 -0400

    Assignment completed

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -12,6 +12,31 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
+const getUsers = (request, response) => {
+  respondJSON(request, response, 200, { users });
+};
+
+const getUsersMeta = (request, response) => {
+  respondJSON(request, response, 200);
+};
+
+const addUser = (request, response, body) => {
+  if (!body.name || !body.age) { // Missing parameters
+    return respondJSON(request, response, 400, { message: 'Cannot add user unless both name and age are specified', id: 'addUserMissingParams' });
+  }
+
+  if (!users[body.name]) { // New name
+    users[body.name] = {
+      name: body.name,
+      age: body.age,
+    };
+    return respondJSON(request, response, 201, { message: 'User created successfully' });
+  }
+
+  users[body.name].age = body.age; // Updating existing name
+  return respondJSON(request, response, 204);
+};
+
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
@@ -26,6 +51,9 @@ const notFoundMeta = (request, response) => {
 };
 
 module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
   notFound,
   notFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 // Standard setup code
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// This function mostly copied from body-parse-example
+const addUser = (request, response) => {
+  const body = [];
+
+  request.on('error', (err) => { // function to call if error occurred
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => { // Called evey time we recieve a piece of data
+    body.push(chunk);
+  });
+
+  request.on('end', () => { // Called when all data has been received
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    jsonHandler.addUser(request, response, bodyParams);
+  });
+};
 
 const urlDict = {
   GET: {
@@ -14,13 +37,19 @@ const urlDict = {
     notFound: jsonHandler.notFound,
   },
   HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
     notFound: jsonHandler.notFoundMeta,
+  },
+  POST: {
+    '/addUser': addUser,
+    notFound: jsonHandler.notFound,
   },
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  // This line is weird but it DOES work in all cases
   const handlerFunction = urlDict[request.method]
     ? urlDict[request.method][parsedUrl.pathname] : urlDict.HEAD.notFound;
 
